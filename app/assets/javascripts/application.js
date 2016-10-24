8064a1a32c1cbc2a15052a5936544708727f1068
Benjamin Phang <BenPhang@Benjamins-MacBook-Pro.local>
2016-10-24 14:37:42 +0800

    fixed datepicker functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,41 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
-@import "bootstrap.js";
-@import "jquery.js";
-
- $( function() {
-    var dateFormat = "mm/dd/yy",
-      from = $( "#from" )
-        .datepicker({
-          defaultDate: "+1w",
-          changeMonth: true,
-          numberOfMonths: 3
-        })
-        .on( "change", function() {
-          to.datepicker( "option", "minDate", getDate( this ) );
-        }),
-      to = $( "#to" ).datepicker({
-        defaultDate: "+1w",
-        changeMonth: true,
-        numberOfMonths: 3
-      })
-      .on( "change", function() {
-        from.datepicker( "option", "maxDate", getDate( this ) );
-      });
- 
-    function getDate( element ) {
-      var date;
-      try {
-        date = $.datepicker.parseDate( dateFormat, element.value );
-      } catch( error ) {
-        date = null;
-      }
- 
-      return date;
-    }
-  } );
+// = require jquery
+// = require jquery_ujs
+// = require turbolinks
+// = require bootstrap-sprockets
+// = require_tree .
